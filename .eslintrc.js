d51a8fb022a0e3106c4a60eb86930b032b79007f
Hanna Lushchyk <hanna.lushchyk@itrexgroup.com>
2022-10-25 16:56:06 +0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,76 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  settings: {
+    'import/extensions': ['.js', '.jsx'],
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx']
+      }
+    }
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    quotes: [2, 'single'],
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'none'
+      }
+    ],
+
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id']
+        }
+      }
+    ],
+
+    'jsx-a11y/anchor-is-valid': 0,
+
+    'comma-dangle': 0,
+    'dot-notation': 0,
+
+    'no-shadow': 'off',
+    'no-unused-vars': 'off',
+
+    'react/prop-types': 0,
+    'react/require-default-props': 'off',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 0,
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx'] }],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function'
+      }
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        mjs: 'never'
+      }
+    ]
+  }
+};
