a8f72c3ad2f465f6670441aceb1fbfdfee8a1d85
Hanna Lushchyk <hanna.lushchyk@itrexgroup.com>
2022-11-08 16:29:24 +0400

    [Feat] Registration form

--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,20 @@
+import RegisterForm from 'components/forms/RegisterForm';
+
 const Register = () => {
-  return <h1>Register Page</h1>;
+  const register = (data) => {
+    alert(JSON.stringify(data));
+  };
+
+  return (
+    <div className="bwm-form mt-5">
+      <div className="row">
+        <div className="col-md-5 mx-auto">
+          <h1 className="page-title">Register</h1>
+          <RegisterForm onSubmit={register} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
