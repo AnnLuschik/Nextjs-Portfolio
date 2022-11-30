e075b72eaad67e535dafd4803d4d8ae020a582d2
Hanna Lushchyk <hanna.lushchyk@itrexgroup.com>
2022-11-30 19:23:24 +0400

    Change env config

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const dev = process.env.NODE_ENV !== 'production';
-
-module.exports = {
-  env: {
-    BASE_URL: dev
-      ? 'http://localhost:3000/graphql'
-      : 'https://nextjs-portfolio-gold-beta.vercel.app/graphql'
-  }
-};
