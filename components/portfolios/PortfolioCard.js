dbb10804defb183bc6f04fabd2201e076d75f68a
Hanna Lushchyk <hanna.lushchyk@itrexgroup.com>
2022-12-09 12:32:20 +0400

    [Fix] Overflow in portfolio card

--- components/portfolios/PortfolioCard.js
+++ components/portfolios/PortfolioCard.js
@@ -1,4 +1,4 @@
-import { formatDate } from 'helpers';
+import { formatDate, shortify } from 'helpers';
 
 const PortfolioCard = ({ data }) => {
   return (
@@ -6,7 +6,7 @@ const PortfolioCard = ({ data }) => {
       <div className="card-body">
         <h5 className="card-title">{data.title}</h5>
         <h6 className="card-subtitle mb-2 text-muted">{data.jobTitle}</h6>
-        <p className="card-text fs-6">{data.description}</p>
+        <p className="card-text fs-6">{shortify(data.description, 150)}</p>
       </div>
       <div className="card-footer no-border">
         <small className="text-muted">
