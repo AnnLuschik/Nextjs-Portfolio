a42c1be3d7c022d3a18f8dd60a103fcaf5ce12b3
Hanna Lushchyk <hanna.lushchyk@itrexgroup.com>
2022-12-21 13:18:03 +0400

    [Fix] Remove unnecessary link

--- components/shared/Hero.js
+++ components/shared/Hero.js
@@ -1,38 +1,37 @@
 import Image from 'next/image';
 
-const Hero = () => (
-  <section className="fj-hero">
-    <div className="fj-hero-wrapper row">
-      <div className="hero-left col-md-6">
-        <h1 className="white hero-title">
-          Hey I&#39;m John. Enthusiastic front end developer
-        </h1>
-        <h2 className="white hero-subtitle">Check my portfolio</h2>
-        <div className="button-container">
-          <a
-            href="https://github.com/AnnLuschik"
-            className="btn btn-main bg-blue ttu"
-          >
-            See my work
-          </a>
+const Hero = () => {
+  return (
+    <section className="fj-hero">
+      <div className="fj-hero-wrapper row">
+        <div className="hero-left col-md-6">
+          <h1 className="white hero-title">
+            Hey I&#39;m John. Enthusiastic front end developer
+          </h1>
+          <h2 className="white hero-subtitle">Check my portfolio</h2>
+          <div className="button-container">
+            <a
+              href="https://github.com/AnnLuschik"
+              className="btn btn-main bg-blue ttu"
+            >
+              See my work
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="hero-right col-md-6">
-        <div className="hero-image-container">
-          <a className="grow hero-link">
+        <div className="hero-right col-md-6">
+          <div className="hero-image-container">
             <Image
-              className="hero-image"
+              className="hero-image grow"
               src="https://i.udemycdn.com/course/750x422/1652608_662b_8.jpg"
               width="350"
               height="350"
               alt=""
-              priority
             />
-          </a>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Hero;
